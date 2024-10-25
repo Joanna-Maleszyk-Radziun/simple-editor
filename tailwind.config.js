6ed5ca864e8b38216b580bc7b63d5f5092dbad2f
Joanna-Maleszyk-Radziun <artmaleszyk@gmail.com>
2024-10-25 15:18:40 +0200

    added tailwind Transitions & Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
       colors: {
         'wtf': '#557788'
       },
       fontFamily: {
         'poppin': ['Poppins', 'sans-serif'],
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
     },
   },
   plugins: [],
